ea5bbb2cdb99f0b3dfe096976d0a18b4f77a9be1
Zanirina <beketovazarina.bz@gmail.com>
2025-10-06 23:45:16 +0500

    Add shaders folder with PBR shader files

--- shaders/pbr.fs
+++ shaders/pbr.fs
@@ -4,73 +4,31 @@ out vec4 FragColor;
 in vec3 FragPos;
 in vec3 Normal;
 
-uniform vec3 camPos;
-
-/* material */
 uniform vec3 albedo;
 uniform float metallic;
 uniform float roughness;
 uniform float ao;
 
-/* lights (we use 4 lights) */
+uniform vec3 camPos;
 uniform vec3 lightPositions[4];
 uniform vec3 lightColors[4];
 
 const float PI = 3.14159265359;
 
-// Fresnel-Schlick
 vec3 fresnelSchlick(float cosTheta, vec3 F0)
 {
     return F0 + (1.0 - F0) * pow(1.0 - cosTheta, 5.0);
-}
-
-// Normal Distribution Function (GGX)
-float DistributionGGX(vec3 N, vec3 H, float roughness)
-{
-    float a = roughness*roughness;
-    float a2 = a*a;
-    float NdotH = max(dot(N, H), 0.0);
-    float NdotH2 = NdotH*NdotH;
-
-    float nom   = a2;
-    float denom = (NdotH2 * (a2 - 1.0) + 1.0);
-    denom = PI * denom * denom;
-
-    return nom / max(denom, 0.000001);
-}
-
-// Geometry (Schlick-GGX)
-float GeometrySchlickGGX(float NdotV, float roughness)
-{
-    float r = (roughness + 1.0);
-    float k = (r*r) / 8.0;
-    float nom   = NdotV;
-    float denom = NdotV * (1.0 - k) + k;
-    return nom / denom;
-}
-
-float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness)
-{
-    float NdotV = max(dot(N, V), 0.0);
-    float NdotL = max(dot(N, L), 0.0);
-    float ggx2 = GeometrySchlickGGX(NdotV, roughness);
-    float ggx1 = GeometrySchlickGGX(NdotL, roughness);
-    return ggx1 * ggx2;
 }
 
 void main()
 {
     vec3 N = normalize(Normal);
     vec3 V = normalize(camPos - FragPos);
-    vec3 R = reflect(-V, N);
 
-    // calculate reflectance at normal incidence; if metallic, use albedo as F0
     vec3 F0 = vec3(0.04);
     F0 = mix(F0, albedo, metallic);
 
-    // ambient (approx)
     vec3 Lo = vec3(0.0);
-
     for(int i = 0; i < 4; ++i)
     {
         vec3 L = normalize(lightPositions[i] - FragPos);
@@ -79,30 +37,25 @@ void main()
         float attenuation = 1.0 / (distance * distance);
         vec3 radiance = lightColors[i] * attenuation;
 
-        float NDF = DistributionGGX(N, H, roughness);
-        float G   = GeometrySmith(N, V, L, roughness);
-        vec3 F    = fresnelSchlick(max(dot(H, V), 0.0), F0);
+        float NDF = 1.0;
+        float G = 1.0;
+        vec3 F = fresnelSchlick(max(dot(H, V), 0.0), F0);
 
-        vec3 numerator    = NDF * G * F;
-        float denominator = 4.0 * max(dot(N, V), 0.0) * max(dot(N, L), 0.0) + 0.0001;
-        vec3 specular = numerator / denominator;
+        vec3 nominator = NDF * G * F;
+        float denominator = 4.0 * max(dot(N, V), 0.0) * max(dot(N, L), 0.0) + 0.001;
+        vec3 specular = nominator / denominator;
 
-        float NdotL = max(dot(N, L), 0.0);
         vec3 kS = F;
         vec3 kD = vec3(1.0) - kS;
         kD *= 1.0 - metallic;
 
+        float NdotL = max(dot(N, L), 0.0);
         Lo += (kD * albedo / PI + specular) * radiance * NdotL;
     }
 
-    // ambient term (very simple)
     vec3 ambient = vec3(0.03) * albedo * ao;
-
     vec3 color = ambient + Lo;
-
-    // tone mapping (simple)
     color = color / (color + vec3(1.0));
-    // gamma
     color = pow(color, vec3(1.0/2.2));
 
     FragColor = vec4(color, 1.0);
